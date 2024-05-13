35ed0b7dcf4c4a6e7c2fdc091cef29cf3739a09a
Issey M <isseymatsumoto0911@gmail.com>
2024-05-13 17:10:24 +0900

    added home.html and made main redirect to it

--- .venv/main.py
+++ .venv/main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hell_world():
-    return "<h1> Hello World! </h1>"
+def homepage():
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.run(debug=True)
