c27a7e347749fefc5b578189a7555c8bb06d0736
Issey M <isseymatsumoto0911@gmail.com>
2024-05-13 16:57:32 +0900

    about page

--- .venv/main.py
+++ .venv/main.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hell_world():
-    return "Hello, World!"
+    return "<h1> Hello World! </h1>"
+
+@app.route('/about')
+def about_page():
+    return "<h1> About page </h1>"
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
