5219f4d8ce1c1ecc282b435d280c881b0d545692
Issey M <isseymatsumoto0911@gmail.com>
2024-05-16 20:00:32 +0900

    divided up

---
+++ .venv/models.py
@@ -0,0 +1,10 @@
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    barcode = db.Column(db.String(12), unique=True, nullable=False)
+    description = db.Column(db.String(1024), nullable=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
+    
