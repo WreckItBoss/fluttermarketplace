bd32b53af85e570e8b6a3e8684ff39921f7bb295
Issey M <isseymatsumoto0911@gmail.com>
2024-05-16 18:05:40 +0900

    added stuff to db

--- .venv/setup_db.py
+++ .venv/setup_db.py
@@ -1,4 +1,11 @@
 from main import app, db
+from main import Item
 
 with app.app_context():
     db.create_all()
+    item1 = Item(name = "iPhone 10", price = 500, barcode = '101010448943', description = "desc")
+    item2 = Item(name = "Laptop", price = 1000, barcode = '105510448943', description = "description")
+    db.session.add(item1)
+    db.session.add(item2)
+    db.session.commit()
+    print(Item.query.all()) 
