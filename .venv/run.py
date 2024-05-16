040b577b45a2f3e829493ae5281e58583538fb10
Issey M <isseymatsumoto0911@gmail.com>
2024-05-16 20:08:56 +0900

    organized

--- .venv/run.py
+++ .venv/run.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
-
+from market import app
+from market import db
 if __name__=='__main__':
     with app.app_context():
         db.create_all()
