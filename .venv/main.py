18e28955eb13a185ad150a8e2c92b4380739e02d
Issey M <isseymatsumoto0911@gmail.com>
2024-05-16 18:15:13 +0900

    added drop all

--- .venv/main.py
+++ .venv/main.py
@@ -5,11 +5,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
 class Item(db.Model):
-    id = db.Column(db.Integer(), primary_key = True)
-    name = db.Column(db.String(length = 30), nullable = False)
-    price = db.Column(db.Integer(), nullable = False)
-    barcode = db.Column(db.String(length = 12), nullable = False, unique = True)
-    description = db.Column(db.String(length = 1024), nullable = False, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    barcode = db.Column(db.String(12), unique=True, nullable=False)
+    description = db.Column(db.String(1024), nullable=True)
 
     def __repr__(self):
         return f'Item {self.name}'
--- .venv/setup_db.py
+++ .venv/setup_db.py
@@ -2,6 +2,7 @@ from main import app, db
 from main import Item
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
     item1 = Item(name = "iPhone 10", price = 500, barcode = '101010448943', description = "desc")
     item2 = Item(name = "Laptop", price = 1000, barcode = '105510448943', description = "desc2")
