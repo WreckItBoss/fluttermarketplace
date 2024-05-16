466e37a08a0e4ec6fe9c5443a053323a8c481442
Issey M <isseymatsumoto0911@gmail.com>
2024-05-16 20:44:46 +0900

    added user and added relationship between user and item

--- .venv/market/models.py
+++ .venv/market/models.py
@@ -2,7 +2,11 @@ from market import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(length = 30), unique = True, nullable = False)
-    
+    email_address = db.Column(db.String(length = 50), unique = True, nullable = False)
+    password_hash = db.Column(db.String(length = 60), nullable = False)
+    budget = db.Column(db.Integer(), nullable = False, default = 1000)
+    items = db.relationship('Item', backref = 'owned_user', lazy = True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +14,7 @@ class Item(db.Model):
     price = db.Column(db.Float, nullable=False)
     barcode = db.Column(db.String(12), unique=True, nullable=False)
     description = db.Column(db.String(1024), nullable=True)
+    owner = db.Column(db.Integer(), db.ForeigKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
